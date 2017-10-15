5ab44e6c41ab8242a3c755b6f5a7519a179b4dbf
Nathan Wisnoski <wisnoski@indiana.edu>
2017-10-15 19:15:06 -0400

    Fixed typos in environmental fluctuation and consumption functions

--- metacom-model.R
+++ metacom-model.R
@@ -3,12 +3,12 @@ require(vegan)
 rm(list=ls())
 
 c.ij <- function(H.i, E.j){
-  return(1.5 - abs(H.i - E.j))
+  return((1.5 - abs(H.i - E.j))/10)
 }
 
 E.j <- function(t){
   0.5*env.ampl*(
-    sin((2*pi/env.period)*t + 1 + (1:M)*2*pi/M) + 1)
+    sin((1:M)*2*pi/M + (2*pi/env.period)*t) + 1)
 }
 
 migrate <- function(N){
@@ -19,8 +19,8 @@ div.part <- function(M){
   M.pa <- decostand(M, method = 'pa')
   alpha <- rowSums(M.pa)
   gamma <- sum(colSums(M.pa) >= 1)
-  beta <- gamma / alpha
-  return(list(alpha = alpha, beta = beta, gamma = gamma))
+  beta <- gamma - alpha
+  return(c(mean(alpha), mean(beta), gamma))
 }
 
 run.sim <- function(a){
@@ -59,30 +59,34 @@ stepnum <- 10000
 dt <- 0.1
 timesteps <- seq(1,stepnum,dt)
 
-M <- 10 # number of patches
-S <- 20 # number of species
+M <- 30 # number of patches
+S <- 9 # number of species
 n.ij <- 10 # number of individuals in each patch at t0
-r.j <- 30 # number of resources at t0
+r.j <- 9 # number of resources at t0
 
-I.j <- 10 # resource inputs
-l.j <- 3 # resource loss
+I.j <- 150 # resource inputs
+l.j <- 10 # resource loss
 
 e.ij <- 0.2 # conversion efficiencies
-m.ij <- 0.3 # mortality rates
+m.ij <- 0.2 # mortality rates
 
-a <- .1 # dispersal rate
+a <- .01 # dispersal rate
 ext <- 0.1 # extinction threshold
 
-env.period <- 50000 # period of env sinusoidal fluctuations
+env.period <- 40000 # period of env sinusoidal fluctuations
 env.ampl <- 1 # amplitude of envrionment sinusoidal fluctuations
 env.optima <- 1 - seq(0, env.ampl, by=env.ampl/(S-1))
 
 ## Run Simulation
 mc.dynamics <- run.sim(a = a)
+#mc.dynamics <- mc.dynamics[seq(1, length(timesteps), length.out = stepnum)]
 
 sp.dyn <- sapply(mc.dynamics, colSums)
 sp.dyn <- (t(sp.dyn))
-#head(sp.dyn)
+head(sp.dyn)
 matplot(sp.dyn, type = 'l')
-div.part(mc.dynamics[[10000]])
-
+div.traj <- sapply(mc.dynamics, FUN = div.part)
+div.traj <- t(div.traj)
+colnames(div.traj) <- c("alpha", "beta", "gamma")
+matplot(div.traj, type = 'l', col = c("red","green","blue"), lwd = 2)
+div.traj[80000,]
