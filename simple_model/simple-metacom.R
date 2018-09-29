166733c5cd5c30f390988403b79c3cd0a52c39ae
Nathan Wisnoski <wisnoski@indiana.edu>
2018-09-28 20:04:55 -0400

    added env varition and better file organization for pos neg covary

--- simple_model/simple-metacom.R
+++ simple_model/simple-metacom.R
@@ -17,18 +17,27 @@ dt <- 1    # precision for model integration (step size)
 M <- 20 # Number of sites
 S <- 20 # Number of species
 ext <- .01 # extinction thresh
-disturb <- 0.00
-
+disturb <- 0.001
+env.type <- "static" # "static", "fluctuating", "random" okay  
+spatial.synchrony <- 0 #range from 0 to 1, what fraction of patches have the same environment
 
 envs <- 1 # Number of environmental variables
 
 
 # set up environment (E)
 E <- matrix(seq(0, 1, length.out = M), nrow = M, ncol = envs)
+env.ampl <- 1 # amplitude of env variability [0,1]
+env.period <- 1000 # bigger numbers, slower oscilations, more static env
 
+E.j <- function(t, synch = spatial.synchrony, stoch = F){
+  0.5*env.ampl*(
+    sin((1:M)*2*pi/(M*(1-synch)) + (2*pi/env.period)*t) + 1) + ifelse(stoch, abs(rnorm(1, sd = .1)), 0)
+  
+}
 
-
-
+env.dyn <- matrix(NA, nrow = tsteps, ncol = M)
+for(t in 1:tsteps){env.dyn[t,] = E.j(t)}
+autoplot.zoo(env.dyn, facet = NULL) + theme_minimal()
 ###############################################################################
 # Define metacommunity functions
 
@@ -63,14 +72,16 @@ nbreadth <- .5 # as approach infinity, metacom approaches neutrality
 max.R <- 1.2
 a <- 4e-4 # strength of competition
 
-d <- rep(.3, S) # Dispersal rates
+d <- rep(.05, S) # Dispersal rates
 decay <- rep(.00001, S) # Decay rate of dormant propagules
 dorm <- rep(0.5, S) # Propensity to enter dormancy
 activ <- rep(0.1, S) # Reactivation rate
 
+ddcov <- 0 # 0 for negative, 1 for positive
+
 ###############################################################################
 
-dorm.grad <- c(0, .001, .005, 0.01, 0.05, 0.2, 0.6, 0.9, 1)
+dorm.grad <- c(0, .001, .005, 0.01, 0.02, 0.05, 0.07, 0.08, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1)
 disp.grad <- c(0, .001, .005, 0.01, 0.02, 0.05, 0.07, 0.08, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1)
 # dorm.grad <- c(0, .01, .1, 1)
 # disp.grad <- c(0, 0.01, 0.1, 1)
@@ -115,6 +126,8 @@ for(dorm in dorm.grad){
         
         # calculates growth rates for each species (rows) in each site (cols)
         # dimensions = S x M
+        if(env.type == "fluctuating") E <- matrix(E.j(t), nrow = M, ncol = 1)
+        if(env.type == "random") E <- matrix(runif(M), nrow = M, ncol = 1)
         R.t <- apply(X = E, MARGIN = 1, FUN = R.jx, max.R, opt.envs, nbreadth)
         
         # dormancy transitions
@@ -132,7 +145,7 @@ for(dorm in dorm.grad){
         
         # calculate immigration and then remove emigrants
         N.t3 <- N.t2 + disperse(d, N.t2, M) - (d * N.t2)
-        D.t3 <- D.t2 + disperse(d, D.t2, M) - (d * D.t2)
+        D.t3 <- D.t2 + ddcov*(disperse(d, D.t2, M) - (d * D.t2))
         
         # patch disturbance
         N.t3[, which(rbernoulli(M, p = disturb) == 1)] <- 0
@@ -165,9 +178,9 @@ for(dorm in dorm.grad){
   # specnumber(colSums(comm))
   # ord <- rda(comm ~ E); plot(ord)
   #percent.env <- as.numeric(eigenvals(ord)[1]/sum(eigenvals(ord)))
-  (alpha <- vegetarian::d(comm, lev = "alpha"))
-  (beta <- vegetarian::d(comm, lev = "beta"))
-  (gamma <- vegetarian::d(comm, lev = "gamma"))
+  (alpha <- vegetarian::d(comm, lev = "alpha", q = 1))
+  (beta <- vegetarian::d(comm, lev = "beta", q = 1))
+  (gamma <- vegetarian::d(comm, lev = "gamma", q = 1))
   
   # write out dispersal, dormancy, and diversity
   out.sum[i,] <- c(d, dorm, alpha, beta, gamma)
@@ -183,25 +196,27 @@ as.data.frame(out.sum) %>%
   mutate(Dormancy = factor(Dormancy, levels = dorm.grad, ordered = T)) %>% 
   ggplot(aes(Dispersal, Diversity, color = Dormancy)) +
   # geom_point(size = 2, alpha = 0.5) +
-  geom_line(alpha = 0.6) + 
+  geom_line(alpha = 0.8) + 
   facet_grid(Scale ~ ., scales = "free_y") +
   theme_minimal() + 
-  scale_x_continuous(limits = c(0,0.5)) +
+  scale_x_continuous(limits = c(0,1)) +
   scale_color_viridis(discrete = T, begin = .2, end = .8, direction = -1) + 
-  theme(panel.grid = element_blank(), panel.background = element_rect(color = "grey90")) +
-  ggsave(file.path("figures", "diversity-dispersal", paste0("diversity-dispersal_dist",disturb,".png")), 
+  theme(panel.grid = element_blank(), panel.background = element_rect(color = "grey90"),
+        legend.text = element_text(size = 8), legend.key.size = unit(0.3, "cm")) +
+  ggsave(file.path("figures", "diversity-dispersal", paste0(ifelse(ddcov, "pos", "neg"),"-ddcov"),
+                   paste0("diversity-dispersal_",env.type,"_dist",disturb,"_period",env.period,".png")), 
          width = 4, height = 4, units = "in", dpi = 500)
 
 
-as.data.frame(out.sum) %>% 
+as.data.frame(out.sum) %>%
   gather(Alpha, Beta, Gamma, key = Scale, value = Diversity) %>%
-  mutate(Dormancy = factor(Dormancy, levels = dorm.grad, ordered = T)) %>% 
-  filter(Scale == "Beta") %>% 
+  mutate(Dormancy = factor(Dormancy, levels = dorm.grad, ordered = T)) %>%
+  filter(Scale == "Beta") %>%
   ggplot(aes(Dispersal, Diversity, color = Dormancy)) +
   # geom_point(size = 2, alpha = 0.5) +
-  geom_line(alpha = 0.6) + 
-  theme_minimal() + 
+  geom_line(alpha = 0.6) +
+  theme_minimal() +
   scale_x_continuous(limits = c(0,0.2)) +
-  scale_y_continuous(limits = c(1,5)) +
-  scale_color_viridis(discrete = T, begin = .2, end = .8, direction = -1) + 
+  scale_y_log10() +
+  scale_color_viridis(discrete = T, begin = .2, end = .8, direction = -1) +
   theme(panel.grid = element_blank(), panel.background = element_rect(color = "grey90"))
